b10f5d45e5118f733458db24382fd107efeb5de0
warahagi <hagi.02180218@gmail.com>
2026-05-08 08:52:36 +0900

    feat: add mobile support and PWA features

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'quick-snippets-v1';
+const ASSETS = [
+  'index.html',
+  'flashcard.html',
+  'snippets.json',
+  'icon-512.png'
+];
+
+// Install Event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate Event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
